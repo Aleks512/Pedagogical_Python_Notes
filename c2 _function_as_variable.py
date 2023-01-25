dfa441c20a45f32a9ed0f8fc73a8c0d5508d153b
Aleksandra <54075858+Aleks512@users.noreply.github.com>
2023-01-25 14:43:42 +0100

    Update c2 _function_as_variable.py

--- c2 _function_as_variable.py
+++ c2 _function_as_variable.py
@@ -3,5 +3,5 @@ from concept_1 import make_title, power
 some_variable = make_title
 other_variable = power
 
-print(some_variable("cats are the best ")) # Cats Are The Best
-print(other_variable(3, 4)) # 81
+print(some_variable("cats are the best "))
+print(other_variable(3, 4))
