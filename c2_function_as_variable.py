96b57b28e9fffc46da05c6a50d4b1787799346bd
Aleksandra Cichocka <54075858+Aleks512@users.noreply.github.com>
2023-01-25 15:33:29 +0100

    printing the reslults of functions as variables

--- c2_function_as_variable.py
+++ c2_function_as_variable.py
@@ -1,8 +1,13 @@
+# Assign function to variable
 from concept_1 import make_title, power
 
 some_variable = make_title
 other_variable = power
 
-if __name__ == '__main':
-    print(some_variable("cats are the best "))
-    print(other_variable(3, 4))
+
+result_title = some_variable("cats are the best ")
+result_power = other_variable(3, 4)
+
+if __name__ == "__main__":
+    print(result_title)  # Cats Are The Best
+    print(result_power) #  81
